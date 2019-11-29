f6ece67636bcfba3369e4495d8cb8bee6b7507d6
Yuki Shimizu <yucchini.yuki1014@gmail.com>
2019-11-29 14:46:01 +0900

    Create create action and private method

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -6,8 +6,25 @@ class TasksController < ApplicationController
   end
 
   def new
+    @task = Task.new
   end
 
   def edit
   end
+
+  def create
+    # 安全化されたtaskパラメータをtask_paramsメソッドで取得して、それを使ってTaskオブジェクトを作成する
+    task = Task.new(task_params)
+    task.save! # DBに保存
+    # 一覧画面に遷移
+    redirect_to tasks_url, notice: "タスク「#{task.name}」を登録しました。"
+  end
+
+  private
+
+    # フォームからリクエストパラメーターとして送られてきた情報が想定通りの形であることをチェックし、nameとdesctiptionの情報だけを抜き取る
+    # 不正なデータが登録・更新がされてしまうことを防ぐ
+    def task_params
+      params.require(:task).permit(:name, :desctiption)
+    end
 end
