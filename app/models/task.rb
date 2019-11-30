e403cbb21135f2558ce8d08324ce526f8f6f4278
Yuki Shimizu <yucchini.yuki1014@gmail.com>
2019-11-30 19:09:27 +0900

    Add validations

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,13 @@
 class Task < ApplicationRecord
-    validates :name, presence: true
+    validates :name, presence: true, length: { maximum: 30 }
+    validate :validate_name_not_includeing_comma
+
+    private
+      # 検証エラーを発見したら、errorsにエラー内容を格納する
+      def validate_name_not_includeing_comma
+        # nameにカンマが含まれていたらerrorsにエラー内容をaddする
+        errors.add(:name, 'にカンマを含めることはできません') if name&.include?(',')
+      end
+    
+    
 end
