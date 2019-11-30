54eeaad022babccc39ccd722f34c51e2981a1ed6
Yuki Shimizu <yucchini.yuki1014@gmail.com>
2019-11-30 16:31:31 +0900

    Fix rspec setting

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,18 @@ module App
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
+    config.generators do |g|
+      g.test_framework :rspec,
+      # テストDBにレコードを作成するファイルの作成をスキップする
+      fixtures: false,
+      # falseはビュースペックを作成しない。代わりにフィーチャースペックでUIをテストする
+      view_specs: false,
+      # falseはヘルパーファイル用のスペックを作成しないことを指定する。Rspecを自在に操れるようになってきたらtrueにしてヘルパーファイルをテストする
+      helper_specs: false,
+      # falseはroutes.rb用のスペックファイルの作成を省略する。アプリが大きくなり、ルーティングが複雑化してきたらtrueにする
+      routing_specs: false
+    end
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
