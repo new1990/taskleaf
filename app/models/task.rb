b97de7d664ff49a3810f271da369e72b6b625746
Yuki Shimizu <yucchini.yuki1014@gmail.com>
2019-12-01 15:39:24 +0900

    Add associations

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,6 +2,9 @@ class Task < ApplicationRecord
     validates :name, presence: true, length: { maximum: 30 }
     validate :validate_name_not_includeing_comma
 
+    # 各TaskはUserに1つだけ属する
+    belongs_to :user
+
     private
 
       # 検証エラーを発見したら、errorsにエラー内容を格納する
