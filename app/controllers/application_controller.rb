08ada9b062a9a1197f5c6b8d8c40ba571e3afd43
Yuki Shimizu <yucchini.yuki1014@gmail.com>
2019-12-01 15:13:24 +0900

    Add current_user helper method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 
 class ApplicationController < ActionController::Base
+    # ログインしているユーザーを取得する処理は頻繁に必要なため、すべてのコントローラーから利用できるようにする
+    helper_method :current_user
+
+    private
+
+      def current_user
+        @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
+      end
 end
