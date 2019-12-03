0d513f2fd54a03ff6a0af2934180babcdea1294b
Yuki Shimizu <yucchini.yuki1014@gmail.com>
2019-12-03 16:32:07 +0900

    Refactor code by 'let'

--- spec/system/tasks_spec.rb
+++ spec/system/tasks_spec.rb
@@ -2,27 +2,40 @@ require 'rails_helper'
 
 describe "タスク管理機能", type: :system do
     describe "一覧表示機能" do
+        # user_aとuser_bを定義
+        let(:user_a) { FactoryBot.create(:user, name: 'ユーザーA', email: 'a@example.com')}
+        let(:user_b) { FactoryBot.create(:user, name: 'ユーザーB', email: 'b@example.com')}
+
         before do
-            # ユーザーAを作成
-            user_a = FactoryBot.create(:user, name: 'ユーザーA', email: 'a@example.com')
             # 作成者がユーザーAであるタスクを作成しておく
             FactoryBot.create(:task, name: '最初のタスク', user: user_a)
+
+            # 共通の処理
+            # ユーザーAでログインする visitでURLにアクセス
+            visit login_path
+            # fill_inでテキスト入力
+            fill_in 'メールアドレス', with: login_user.email
+            fill_in 'パスワード', with: login_user.password
+            click_button 'ログインする'
         end
 
         context "ユーザーAがログインしているとき" do
-            before do
-                # ユーザーAでログインする visitでURLにアクセス
-                visit login_path
-                # fill_inでテキスト入力
-                fill_in 'メールアドレス', with: 'a@example.com', match: :first
-                fill_in 'パスワード', with: 'password', match: :first
-                click_button 'ログインする'
-            end
+            let(:login_user) { user_a }
 
             it 'ユーザーAが作成したタスクが表示される' do
                 # 作成済みのタスクの名称が画面上に表示されていることを確認
                 expect(page).to have_content '最初のタスク'
             end
         end
+
+        context "ユーザーBがログインしているとき" do
+            let(:login_user) { user_b }
+
+            it 'ユーザーAが作成したタスクが表示されない' do
+                # ユーザーAが作成したタスクの名称が画面上に表示されていないことを確認
+                expect(page).to have_no_content '最初のタスク'
+            end
+        end
+        
     end
 end
