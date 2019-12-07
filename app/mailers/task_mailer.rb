b8b7177bb3eb13b1c2643205a457664a248a3137
Yuki Shimizu <yucchini.yuki1014@gmail.com>
2019-12-07 21:12:43 +0900

    Add default from

--- app/mailers/task_mailer.rb
+++ app/mailers/task_mailer.rb
@@ -1,11 +1,13 @@
 class TaskMailer < ApplicationMailer
-  # タスクの登録の通知メール
-  def creation_email(task)
-    @task = task
-    mail(
-        subject: 'タスク作成完了メール',
-        to: 'user@example.com',
-        from: 'taskleaf@example.com'
-    )
-  end
+    default from: 'taskleaf@example.com'
+
+    # タスクの登録の通知メール
+    def creation_email(task)
+        @task = task
+        mail(
+            subject: 'タスク作成完了メール',
+            to: 'user@example.com',
+            from: 'taskleaf@example.com'
+        )
+    end
 end
